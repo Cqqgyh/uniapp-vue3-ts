a563503a8c47ebe4715bf78f818063372975d0f3
cqq <945327638@qq.com>
2023-08-25 10:28:42 +0800

    feat: 增加全局样式文件

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createSSRApp } from 'vue'
 import pinia from './store/index'
 import App from './App.vue'
+import './style/index.css'
 export function createApp() {
   const app = createSSRApp(App)
   app.use(pinia)
